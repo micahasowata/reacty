6e16d16e81548a0afc9844c839a773e2aebfaacd
Micah Asowata <andrewgary963@gmail.com>
2024-03-20 21:21:59 +0000

    feat: iterate over an array of values

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import { BriefcaseIcon } from "@heroicons/react/24/outline";
 const App = () => {
 	const name = "Micah";
+	const tech = [
+		{ id: 1, name: "Meta" },
+		{ id: 2, name: "Microsoft" },
+		{ id: 3, name: "Apple" },
+		{ id: 4, name: "Airbnb" },
+		{ id: 5, name: "Stripe" },
+		{ id: 6, name: "Google" },
+		{ id: 7, name: "Netflix" },
+	];
 	return (
 		<>
 			<BriefcaseIcon className="h-16 w-16 text-violet-600" />
 			<h1 className="font-ojuju font-bold">Hello {name}</h1>
-			<p className="font-rethink font-bold">welcome</p>
+			<ul>
+				{tech.map((n) => (
+					<li key={n.id} className="font-rethink">
+						{n.name}
+					</li>
+				))}
+			</ul>
 		</>
 	);
 };
