1f90e5e6d13dd352290e4562cdb960f19cd57145
Micah Asowata <andrewgary963@gmail.com>
2024-03-21 15:10:35 +0000

    feat: use Link in the Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,18 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
 	return (
 		<nav className="px-5">
 			<ul className="flex flex-row justify-between align-middle content-center">
-				<a
-					href="/"
+				<Link
+					to="/"
 					className="flex flex-row justify-between align-middle content-center"
 				>
 					<p className="font-ojuju  font-bold text-2xl p-2">React Jobs</p>
-				</a>
+				</Link>
 				<div className="flex flex-row justify-stretch align-middle content-center">
-					<a href="/" className="p-3 font-rethink font-semibold">
+					<Link to="/jobs" className="p-3 font-rethink font-semibold">
 						Jobs
-					</a>
+					</Link>
 					<a href="/" className="p-3 font-rethink font-semibold">
 						Add Jobs
 					</a>
