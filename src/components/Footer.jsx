c8fd436aa78fbc4901f54a476f05e39d01141f62
Micah Asowata <andrewgary963@gmail.com>
2024-03-21 14:31:28 +0000

    feat: add a footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer className="font-rethink font-semibold flex place-content-center my-5">
+			<sub>
+				<a href="https://github.com/micahasowata">built by micah asowata</a>
+			</sub>
+		</footer>
+	);
+};
+
+export default Footer;
