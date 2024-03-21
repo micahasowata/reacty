62672e9b121e481fcb6f6dce039bc04c8eba7405
Micah Asowata <andrewgary963@gmail.com>
2024-03-21 15:10:05 +0000

    refactor: pass listing details as props

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,13 +1,21 @@
+import { Link } from "react-router-dom";
 import jobs from "../jobs.json";
 import JobCard from "./JobCard";
-
-const Listing = ({ end = jobs.jobs.length - 1 }) => {
+const Listing = ({
+	end = jobs.jobs.length,
+	header = "Browse Jobs",
+	cta,
+	to = "/",
+}) => {
+	if (end > jobs.jobs.length) {
+		end = jobs.jobs.length;
+	}
 	const latestJobs = jobs.jobs.slice(0, end);
 	return (
 		<>
 			<section className="flex flex-col flex-wrap place-content-center bg-slate-200">
 				<h2 className="font-rethink font-semibold text-5xl flex place-content-center p-10">
-					Browse Jobs
+					{header}
 				</h2>
 				<div className="flex flex-wrap justify-around">
 					{latestJobs.map((job) => (
@@ -19,7 +27,7 @@ const Listing = ({ end = jobs.jobs.length - 1 }) => {
 						type="submit"
 						className="font-rethink grid col-start-5 col-end-12 align-middle w-3/5 font-semibold text-2xl underline underline-offset-8  text-neutral-950 px-3 py-2 rounded-sm"
 					>
-						<a href="/">All Jobs</a>
+						<Link to={to}>{cta}</Link>
 					</button>
 				</div>
 			</section>
