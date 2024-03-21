85c1419689196b8962adab1584f4a64db75449c9
Micah Asowata <andrewgary963@gmail.com>
2024-03-21 01:49:07 +0000

    feat: add the hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+	return (
+		<div className="grid grid-cols-12 grid-rows-12">
+			<div className="grid col-start-3 col-end-11 row-start-2 row-end-12 place-content-center">
+				<h2 className="font-ojuju font-light text-7xl pt-8 pb-4">
+					React Jobs.
+				</h2>
+				<h2 className="font-rethink text-7xl pt-4 pb-8">For you.</h2>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
