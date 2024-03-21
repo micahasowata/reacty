9a960e62e634d87110bf08d17fa5abdc58b0d844
Micah Asowata <andrewgary963@gmail.com>
2024-03-21 15:11:28 +0000

    feat: add the jobs page

---
+++ src/pages/Jobs.jsx
@@ -0,0 +1,11 @@
+import Listing from "../components/Listing";
+
+const Jobs = () => {
+	return (
+		<>
+			<Listing end={7} cta={"Go back"} to={"/"} header={"Search Jobs"} />
+		</>
+	);
+};
+
+export default Jobs;
