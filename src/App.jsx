883d596154980b4decc9b6538cfa807fd4840bb1
Micah Asowata <andrewgary963@gmail.com>
2024-03-21 15:10:55 +0000

    feat: add the `/jobs` route

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import Main from "./layout/Main";
 import Home from "./pages/Home";
+import Jobs from "./pages/Jobs";
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
 		<Route path="/" element={<Main />}>
 			<Route index element={<Home />} />
+			<Route path="/jobs" element={<Jobs />} />
 		</Route>,
 	),
 );
