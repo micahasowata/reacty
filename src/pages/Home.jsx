6e3a2acd6bed7b6c1c8fc0621e963fd6d2cad10d
Micah Asowata <andrewgary963@gmail.com>
2024-03-21 14:31:12 +0000

    refactor: move the nav to the main layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,10 @@
 import Card from "../components/Card";
 import Hero from "../components/Hero";
 import Listing from "../components/Listing";
-import Navbar from "../components/Navbar";
 
 const Home = () => {
 	return (
 		<>
-			<Navbar />
 			<Hero />
 			<div className="flex flex-row justify-evenly pt-10 pb-5">
 				<Card title={"For employers"} body={"post jobs here"} cta={"Add Job"} />
